3d446e72c3d3db8fbb80c8abf672ff1ed2c098b9
ashik-75 <ashikex49@gmail.com>
2024-01-20 18:41:13 +0600

    ff

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { Provider } from "react-redux";
-import store from "./app/store.ts";
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<Toaster />
-			<App />
-		</Provider>
+		<Toaster />
+		<App />
 	</React.StrictMode>
 );
