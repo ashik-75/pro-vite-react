9ee7786db22a88ff11893c0105c8ae94eff92228
ashik-75 <ashikex49@gmail.com>
2024-01-21 15:09:05 +0600

    faat:major changes

--- src/features/color/converter.ts
+++ src/features/color/converter.ts
@@ -10,6 +10,37 @@ export function hexToRYB(hex: string) {
 	const { r, g, b } = hexToRGB(hex);
 	return rgbToryb(r, g, b);
 }
+interface RGBObject {
+	r: number;
+	g: number;
+	b: number;
+}
+
+export function objToRgb(colorObj: RGBObject): string {
+	const { r, g, b } = colorObj;
+	return `rgb(${r}, ${g}, ${b})`;
+}
+
+export function rgbToObj(
+	rgbColor: string
+): { r: number; g: number; b: number } | null {
+	// Extract the numerical values from the RGB string
+	const values = rgbColor.match(/\d+/g);
+
+	if (!values || values.length !== 3) {
+		// Handle invalid input
+		return null;
+	}
+
+	// Create an object with 'r', 'g', and 'b' properties
+	const colorObj = {
+		r: parseInt(values[0], 10),
+		g: parseInt(values[1], 10),
+		b: parseInt(values[2], 10),
+	};
+
+	return colorObj;
+}
 
 export function rgbToryb(iRed: number, iGreen: number, iBlue: number) {
 	// Remove the white from the color
@@ -57,3 +88,56 @@ export function rgbToryb(iRed: number, iGreen: number, iBlue: number) {
 
 	return { r: Math.trunc(iRed), y: Math.trunc(iYellow), b: Math.trunc(iBlue) };
 }
+
+export function ryb2rgb(iRed: number, iYellow: number, iBlue: number) {
+	// Remove the whiteness from the color.
+	const iWhite = Math.min(iRed, iYellow, iBlue);
+
+	iRed -= iWhite;
+	iYellow -= iWhite;
+	iBlue -= iWhite;
+
+	const iMaxYellow = Math.max(iRed, iYellow, iBlue);
+
+	// Get the green out of the yellow and blue
+	let iGreen = Math.min(iYellow, iBlue);
+
+	iYellow -= iGreen;
+	iBlue -= iGreen;
+
+	if (iBlue > 0 && iGreen > 0) {
+		iBlue *= 2.0;
+		iGreen *= 2.0;
+	}
+
+	// Redistribute the remaining yellow.
+	iRed += iYellow;
+	iGreen += iYellow;
+
+	// Normalize to values.
+	const iMaxGreen = Math.max(iRed, iGreen, iBlue);
+
+	if (iMaxGreen > 0) {
+		const iN = iMaxYellow / iMaxGreen;
+
+		iRed *= iN;
+		iGreen *= iN;
+		iBlue *= iN;
+	}
+
+	// Add the white back in.
+	iRed += iWhite;
+	iGreen += iWhite;
+	iBlue += iWhite;
+
+	// Save the RGB
+	return { r: Math.trunc(iRed), g: Math.trunc(iGreen), b: Math.trunc(iBlue) };
+}
+
+export const RGBTORYB = (value: string) => {
+	const output = rgbToObj(value);
+	if (output) {
+		return rgbToryb(output.r, output?.g, output?.b);
+	}
+	return { r: 0, y: 0, b: 0 };
+};
--- src/features/color/index.tsx
+++ src/features/color/index.tsx
@@ -1,88 +1,156 @@
-import UploadImage from "./image";
-import { useState, useCallback } from "react";
-import useEyeDropper from "use-eye-dropper";
-import { hexToRGB, hexToRYB } from "./converter";
+import { useState } from "react";
+import { RGBTORYB, objToRgb, ryb2rgb } from "./converter";
+import { ImageColorPicker } from "react-image-color-picker";
+import imageSrc from "../../../public/ai-1.jpg";
+import { cn } from "@/lib/utils";
 
 const Color = () => {
-	const [image, setImage] = useState<File | null>();
-	const { open, isSupported } = useEyeDropper();
-	const [color, setColor] = useState<string>("");
+	const [selected, setSelected] = useState(1);
+	const [color1, setColor1] = useState<string>("");
+	const [color2, setColor2] = useState("");
 
-	const pickColor = useCallback(() => {
-		// Using async/await (can be used as a promise as-well)
-		const openPicker = async () => {
-			try {
-				const color = await open();
-				setColor(color.sRGBHex);
-			} catch (e) {
-				console.log(e);
-				// Ensures component is still mounted
-				// before calling setState
-			}
+	const handleColorPick = (color: string) => {
+		if (selected === 1) {
+			setColor1(color);
+		}
+
+		if (selected === 2) {
+			setColor2(color);
+		}
+	};
+
+	const finalRYB = () => {
+		const r = Math.abs(RGBTORYB(color1).r - RGBTORYB(color2).r);
+		const y = Math.abs(RGBTORYB(color1).y - RGBTORYB(color2).y);
+		const b = Math.abs(RGBTORYB(color1).b - RGBTORYB(color2).b);
+		return {
+			r,
+			y,
+			b,
 		};
-		openPicker();
-	}, [open]);
+	};
 
 	return (
 		<div className="max-w-2xl mx-auto pt-10">
-			<UploadImage image={image} setImage={setImage} />
+			<ImageColorPicker
+				onColorPick={handleColorPick}
+				imgSrc={imageSrc}
+				zoom={1}
+			/>
 
-			{color && (
-				<>
-					<div style={{ padding: "20px", background: color }}>
-						Selected color
-					</div>
-				</>
-			)}
+			<br />
 
-			<div className="my-5">
-				{image ? (
-					<button className="px-5 py-2 rounded-3xl border " onClick={pickColor}>
-						Pick color
-					</button>
-				) : (
-					<span>Pick Image first</span>
-				)}
-			</div>
-
-			<div>{!isSupported() && <p>Eye dropper api not supported</p>}</div>
-
-			{color && (
+			<div className="grid gap-5 grid-cols-3">
 				<div>
-					<div className="flex gap-5">
-						HEX: <pre>{color}</pre>{" "}
-					</div>
-					<div className="flex gap-5">
-						RGB: <pre></pre> {JSON.stringify(hexToRGB(color))}
-					</div>
-					<div className="">
-						<div>
-							RYB: <pre></pre> {JSON.stringify(hexToRYB(color))}
-						</div>
-						<br />
-						<div>
-							<div>RYB in %</div>
-
-							<ul>
-								<li>R: {Math.round((hexToRYB(color).r / 255) * 100)}%</li>
-								<li>Y: {Math.round((hexToRYB(color).y / 255) * 100)}%</li>
-								<li>B: {Math.round((hexToRYB(color).b / 255) * 100)}%</li>
-							</ul>
-						</div>
+					<div
+						onClick={() => setSelected(1)}
+						style={{ padding: "20px", background: color1, width: "200px" }}
+						className={cn(
+							"border rounded-md cursor-pointer",
+							selected === 1 && "border-2 border-zinc-600"
+						)}
+					>
+						Pick First color
 					</div>
 
-					{/* <div
+					{color1 && (
+						<div>
+							<div className="flex gap-5">
+								RGB: <div>{color1}</div>
+							</div>
+							<div className="">
+								<div className="flex gap-2">
+									RYB: <pre></pre> {JSON.stringify(RGBTORYB(color1))}
+								</div>
+								<br />
+								<div>
+									<div>RYB in %</div>
+
+									<ul>
+										<li>R: {Math.round((RGBTORYB(color1).r / 255) * 100)}%</li>
+										<li>Y: {Math.round((RGBTORYB(color1).y / 255) * 100)}%</li>
+										<li>B: {Math.round((RGBTORYB(color1).b / 255) * 100)}%</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
+
+				<div>
+					<div
+						onClick={() => setSelected(2)}
+						style={{ padding: "20px", background: color2, width: "200px" }}
+						className={cn(
+							"border rounded-md cursor-pointer",
+							selected === 2 && "border-2 border-zinc-600 border-spacing-2"
+						)}
+					>
+						Pick Second color
+					</div>
+
+					{color2 && (
+						<div>
+							<div className="flex gap-5">
+								RGB: <div>{color2}</div>
+							</div>
+							<div className="">
+								<div className="flex gap-2">
+									RYB: <pre></pre> {JSON.stringify(RGBTORYB(color2))}
+								</div>
+								<br />
+
+								<div>
+									<div>RYB in %</div>
+
+									<ul>
+										<li>R: {Math.round((RGBTORYB(color2).r / 255) * 100)}%</li>
+										<li>Y: {Math.round((RGBTORYB(color2).y / 255) * 100)}%</li>
+										<li>B: {Math.round((RGBTORYB(color2).b / 255) * 100)}%</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
+				<div>
+					<div
 						style={{
 							padding: "20px",
-							background: `rgb(${hexToRYB(color).r},${hexToRYB(color).y},${
-								hexToRYB(color).b
-							})`,
+							width: "200px",
 						}}
+						className={cn("border rounded-md font-medium")}
 					>
-						Modified Color
-					</div> */}
+						Final Color
+					</div>
+
+					{color1 && color2 && (
+						<div>
+							<div className="flex gap-5">
+								RGB:{" "}
+								<div>
+									{JSON.stringify(
+										objToRgb(ryb2rgb(finalRYB().r, finalRYB().y, finalRYB().b))
+									)}
+								</div>
+							</div>
+							<div className="">
+								<div>RYB: {JSON.stringify(finalRYB())}</div>
+								<br />
+								<div>
+									<div>RYB in %</div>
+
+									<ul>
+										<li>R: {Math.round((finalRYB().r / 255) * 100)}%</li>
+										<li>Y: {Math.round((finalRYB().y / 255) * 100)}%</li>
+										<li>B: {Math.round((finalRYB().b / 255) * 100)}%</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					)}
 				</div>
-			)}
+			</div>
 		</div>
 	);
 };
